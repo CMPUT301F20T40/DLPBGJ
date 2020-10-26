1d624f465338c794cfab3c2d0ae85b449ccafde0
bajwa10 <singhloyaljot2778@gmail.com>
2020-10-25 23:16:23 -0700

    Done first page handling (the login page)
    Created a class User that stores a user's name and password in it. Added
    two buttons, one editText and one password field in the acitiviMain.xml.

--- DLPBGJ (Android)/app/src/main/java/com/example/dlpbgj/MainActivity.java
+++ DLPBGJ (Android)/app/src/main/java/com/example/dlpbgj/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.dlpbgj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText user;
+    private EditText pass;
+    private Button login;
+    private Button signUp;
+    private ArrayList<User> userDataList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userDataList = new ArrayList<>();
+        user = findViewById(R.id.editUserName);
+        pass = findViewById(R.id.editUserPassword);
+        String userName = user.getText().toString();
+        String password = pass.getText().toString();
+        User newUser = new User(userName,password);
+        login = findViewById(R.id.login_button);
+        signUp = findViewById(R.id.signup_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
