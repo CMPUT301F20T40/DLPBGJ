3d0cf90827e7f4abf0ae866c9fb7cd8484a8105a
Siddhant Rawal <srawal@ualberta.ca>
2020-10-26 15:14:22 -0600

    Database bug fixed

--- DLPBGJ (Android)/app/src/main/java/com/example/dlpbgj/MainActivity.java
+++ DLPBGJ (Android)/app/src/main/java/com/example/dlpbgj/MainActivity.java
@@ -2,65 +2,151 @@ package com.example.dlpbgj;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.proto.TargetGlobal;
+import com.google.gson.internal.$Gson$Preconditions;
 
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
 
-    private String TAG = "Sample";
+
+public class MainActivity extends AppCompatActivity {
+    EditText user;
+    EditText pass;
+    TextView msg;
+    Button login;
+    Button signUp;
+    String TAG = "Sample";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseFirestore userDb;
+        user = findViewById(R.id.editUserName);
+        pass = findViewById(R.id.editUserPassword);
+        msg = findViewById(R.id.displayMessage);
+        login = findViewById(R.id.login_button);
+        signUp = findViewById(R.id.signup_button);
+        final String sucess = "Login Successful!";
+        final String fail = "Invalid Login Details";
+        final String noExist = "Please Sign Up";
+        final String exist = "User already exists";
+        final String signUpS = "Successfully Signed Up";
+        final FirebaseFirestore userDb;
 
         //Instance of the User db
         userDb = FirebaseFirestore.getInstance();
 
-        // Creating a top level collection for User database
         final CollectionReference collectionReference = userDb.collection("Users");
-
-        // Creating a Hashmap to store data as key value pairs for storing in db
-        HashMap<String, String> data = new HashMap<>();
-
-        //putting data inside Hashmap as key value pair {User1 :password }
-            data.put("Password", password);
-
-        collectionReference
-
-                .document(username)
-                .set(data)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String userName = user.getText().toString();
+                final String userPass = pass.getText().toString();
+                User newUser = new User(userName,userPass);
+                DocumentReference docRef = collectionReference.document(userName);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
                     @Override
-                    public void onSuccess(Void aVoid) {
-                        // These are a method which gets executed when the task is succeeded
-                        Log.d(TAG, "Data has been added successfully!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // These are a method which gets executed if there’s any problem
-                        Log.d(TAG, "Data could not be added!" + e.toString());
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()){
+                                Map<String,Object> data = document.getData();
+                                final String temp = (String)data.get("Password");
+                                System.out.println(temp);
+                                System.out.println(userPass);
+                                if (Objects.equals(temp, userPass)){
+                                    System.out.println("done");
+                                    msg.setText(sucess);
+                                    //TODO Initialize new activity after login Successful and pass user object in it
+                                }
+                                else {
+                                    msg.setText(fail);}
+                            }
+                            else{
+                                msg.setText(noExist);
+                            }
+                        }
+                        else{
+                            Log.d(TAG,"get failed with ",task.getException());
+                        }
                     }
                 });
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String userName = user.getText().toString();
+                final String userPass = pass.getText().toString();
+                final User newUser = new User(userName,userPass);
+                final HashMap<String,Object> data = new HashMap<>();
+                data.put("Password",userPass);
+                DocumentReference docRef = collectionReference.document(userName);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()){
+                                msg.setText(exist);
+                            }
+                            else {
+                                collectionReference
+                                        .document(userName)
+                                        .set(data)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG,"Data has been added succesfully");
+                                                msg.setText(signUpS);
+                                                //TODO Initialize new activity after login Successful and pass user object in it
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.d(TAG,"Data has been not been added");
+
+                                            }
+                                        });
+                            }
+                        }
+                    }
+                });
+            }
+        });
 
         //for realtime database update
         collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
@@ -71,4 +157,5 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
 }
