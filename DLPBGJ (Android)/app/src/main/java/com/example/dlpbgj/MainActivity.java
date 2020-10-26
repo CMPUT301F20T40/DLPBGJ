3f29d1c9b3011785cfab418ff81e705e36db34b6
bajwa10 <singhloyaljot2778@gmail.com>
2020-10-26 00:25:03 -0700

    User Login and Signup handling done
    First page fully completed only touch ups left. All cases for user login
    and signup handled.

--- DLPBGJ (Android)/app/src/main/java/com/example/dlpbgj/MainActivity.java
+++ DLPBGJ (Android)/app/src/main/java/com/example/dlpbgj/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.nfc.Tag;
 import android.os.Bundle;
 
@@ -12,10 +13,15 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -30,6 +36,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private EditText user;
     private EditText pass;
+    private TextView msg;
     private Button login;
     private Button signUp;
     private ArrayList<User> userDataList;
@@ -43,23 +50,12 @@ public class MainActivity extends AppCompatActivity {
         userDataList = new ArrayList<>();
         user = findViewById(R.id.editUserName);
         pass = findViewById(R.id.editUserPassword);
+        msg = findViewById(R.id.displayMessage);
         String userName = user.getText().toString();
         String password = pass.getText().toString();
-        User newUser = new User(userName,password);
+        final User newUser = new User(userName,password);
         login = findViewById(R.id.login_button);
         signUp = findViewById(R.id.signup_button);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
 
         FirebaseFirestore userDb;
@@ -69,32 +65,74 @@ public class MainActivity extends AppCompatActivity {
 
         // Creating a top level collection for User database
         final CollectionReference collectionReference = userDb.collection("Users");
-
-        // Creating a Hashmap to store data as key value pairs for storing in db
-        HashMap<String, String> data = new HashMap<>();
-
-        //putting data inside Hashmap as key value pair {User1 :password }
-            data.put("Password", password);
-
-        collectionReference
-
-                .document(username)
-                .set(data)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-
+        final HashMap<String, String> data = new HashMap<>();
+        data.put("Password", newUser.getPassword());
+        final DocumentReference docRef = userDb.collection("Users").document(newUser.getUsername());
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @SuppressLint("SetTextI18n")
                     @Override
-                    public void onSuccess(Void aVoid) {
-                        // These are a method which gets executed when the task is succeeded
-                        Log.d(TAG, "Data has been added successfully!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // These are a method which gets executed if there’s any problem
-                        Log.d(TAG, "Data could not be added!" + e.toString());
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if(document.exists()){
+                                msg.setText("Login Successful");
+                            }
+                            else{
+                                msg.setText("Please SignUp!");
+                            }
+                        }
+                        else{
+                            Log.d(TAG, "get failed with", task.getException());
+                        }
                     }
                 });
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if(document.exists()){
+                                msg.setText("Username Already exists!");
+                            }
+                            else{
+                                collectionReference
+                                        .document(newUser.getUsername())
+                                        .set(data)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                // These are a method which gets executed when the task is succeeded
+                                                msg.setText("Successfully Signed Up");
+                                                Log.d(TAG, "Data has been added successfully!");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // These are a method which gets executed if there’s any problem
+                                                msg.setText("Sign Up failed");
+                                                Log.d(TAG, "Data could not be added!" + e.toString());
+                                            }
+                                        });
+                            }
+                        }
+                        else{
+                            Log.d(TAG, "get failed with", task.getException());
+                        }
+                    }
+
+                });
+            }
+        });
 
         //for realtime database update
         collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
