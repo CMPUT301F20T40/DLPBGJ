e5df491aef67b532d605885dbd193faf138f21ef
HarshaanNiles010 <harshaanniles010@gmail.com>
2020-11-06 09:17:23 -0700

    User story 5 WIP

--- DLPBGJ (Android)/app/src/main/java/com/example/dlpbgj/HomePage.java
+++ DLPBGJ (Android)/app/src/main/java/com/example/dlpbgj/HomePage.java
@@ -28,6 +28,7 @@ public class HomePage extends AppCompatActivity {
         Button myBooksButton=findViewById(R.id.MyBooks);
         Button search = findViewById(R.id.Search);
         Button signOut = findViewById(R.id.SignOut);
+        Button request = findViewById(R.id.RequestABook);
 
 
         final String success = "Signed Out!";
@@ -68,7 +69,13 @@ public class HomePage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent RIntent = new Intent(getApplicationContext(),RequestBooks.class);
+                startActivity(RIntent);
+            }
+        });
 
     }
 }
